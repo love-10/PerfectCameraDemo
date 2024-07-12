8390c4473e7d9738bd4aabaddd94b5b046f4139a
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-12 21:36:26 +0800

    1

---
+++ app/src/main/java/com/example/perfectcamerademo/Box.java
@@ -0,0 +1,19 @@
+package com.example.perfectcamerademo;
+
+public class Box {
+     float x;
+     float y;
+     float width;
+     float height;
+     float prop;
+     int label;
+
+    public Box(float x, float y, float width, float height, float prop, int label) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.prop = prop;
+        this.label = label;
+    }
+}
---
+++ app/src/main/java/com/example/perfectcamerademo/T.kt
@@ -0,0 +1,58 @@
+package com.tencent.yolov6ncnn
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Typeface
+
+fun l2l(boxes: MutableList<Box>):MutableList<BoundingBox> {
+    val r = mutableListOf<BoundingBox>()
+    boxes.forEach {
+        r.add(boxtobound(it))
+    }
+    return r
+}
+
+fun boxtobound(box: Box): BoundingBox {
+    return BoundingBox(
+        box.x,
+        box.y,
+        box.x + box.width,
+        box.y + box.height,
+        box.x + box.width / 2f,
+        box.y + box.height / 2f,
+        box.width,
+        box.height,
+        box.prop,
+        box.label,
+        ""
+    )
+}
+
+fun drawBoundingBoxes(bitmap: Bitmap, boxes: List<BoundingBox>): Bitmap {
+    val mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+    val canvas = Canvas(mutableBitmap)
+    val paint = Paint().apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+        strokeWidth = 8f
+    }
+    val textPaint = Paint().apply {
+        color = Color.WHITE
+        textSize = 40f
+        typeface = Typeface.DEFAULT_BOLD
+    }
+    for (box in boxes) {
+        val rect = RectF(
+            box.x1 ,
+            box.y1 ,
+            box.x2 ,
+            box.y2
+        )
+        canvas.drawRect(rect, paint)
+        canvas.drawText(box.clsName, rect.left, rect.bottom, textPaint)
+    }
+    return mutableBitmap
+}
