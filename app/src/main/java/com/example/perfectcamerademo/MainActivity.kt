b497e81fc56a00e167677a91fd942cd6a32056cb
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-14 19:42:47 +0800

    size

--- app/src/main/java/com/example/perfectcamerademo/MainActivity.kt
+++ app/src/main/java/com/example/perfectcamerademo/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.perfectcamerademo
 
-import android.graphics.BitmapFactory
-import android.graphics.ImageFormat
-import android.graphics.Rect
-import android.graphics.YuvImage
+import android.graphics.PixelFormat
 import android.os.Bundle
+import android.view.SurfaceHolder
 import androidx.appcompat.app.AppCompatActivity
 import com.example.perfectcamerademo.databinding.ActivityMainBinding
-import com.otaliastudios.cameraview.CameraListener
-import com.otaliastudios.cameraview.CameraOptions
-import com.otaliastudios.cameraview.size.Size
 import dev.utils.app.ScreenUtils
-import java.io.ByteArrayOutputStream
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
     private lateinit var binding: ActivityMainBinding
     private val yolo6 by lazy {
@@ -27,58 +21,34 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.cameraView.setLifecycleOwner(this)
-        binding.cameraView.addCameraListener(Listener())
-        binding.cameraView.setPreviewStreamSize { mutableListOf(Size(1080, 1920)) }
-        binding.cameraView.frameProcessingMaxWidth = 1080
-        binding.cameraView.frameProcessingMaxHeight = 1920
+
+        binding.cameraview.holder.setFormat(PixelFormat.RGBA_8888)
+        binding.cameraview.holder.addCallback(this)
+
         val params = binding.trackView.layoutParams
         params.width = ScreenUtils.getScreenWidth()
-        params.height = ScreenUtils.getScreenWidth()*16/9
+        params.height = ScreenUtils.getScreenWidth() * 16 / 9
         binding.trackView.layoutParams = params
-        binding.cameraView.addFrameProcessor { frame ->
-            val data = frame.getData<ByteArray>()
-            val yuvImage = YuvImage(
-                data,
-                frame.format,
-                frame.size.width,
-                frame.size.height,
-                null
-            )
-            val jpegStream = ByteArrayOutputStream()
-            yuvImage.compressToJpeg(
-                Rect(
-                    0, 0,
-                    frame.size.width,
-                    frame.size.height
-                ), 100, jpegStream
-            )
-            val jpegByteArray = jpegStream.toByteArray()
-            val bitmap = BitmapFactory.decodeByteArray(
-                jpegByteArray,
-                0, jpegByteArray.size
-            )
-            val time = System.currentTimeMillis()
-//            val boxes =
-//                yolo6.detect(BitmapUtils.reverseByHorizontal(BitmapUtils.rotate(bitmap, -90f)))
-//            val boundingBoxes = boxesToBoundingBoxes(boxes)
-//            binding.trackView.update(boundingBoxes)
-//            Log.d("xxxxx", "time: ${System.currentTimeMillis() - time}")
-//            binding.sampleText.setImageBitmap()
-        }
-
-//        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.d)
-//        val time = System.currentTimeMillis()
-//        val boxes = yolo6.detect(bitmap)
-//        val boundingBoxes: List<BoundingBox> = boxesToBoundingBoxes(boxes)
-//        val bitmap1: Bitmap = drawBoundingBoxes(bitmap, boundingBoxes)
-//        Log.d("xxxxx", "onCreate: ${System.currentTimeMillis() - time}")
-//        binding.sampleText.setImageBitmap(bitmap1)
     }
 
-    private inner class Listener : CameraListener() {
-        override fun onCameraOpened(options: CameraOptions) {
-            binding.cameraView.frameProcessingFormat = ImageFormat.JPEG
-        }
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        yolo6.setOutputWindow(holder.surface)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+    }
+
+    public override fun onResume() {
+        super.onResume()
+
+        yolo6.openCamera(1, 0)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        yolo6.closeCamera()
     }
 }
