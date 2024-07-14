66d56da2435a808764f7eac6470cf982dc8f3dcf
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-14 19:37:26 +0800

    size

--- app/src/main/java/com/example/perfectcamerademo/TheActivity.kt
+++ app/src/main/java/com/example/perfectcamerademo/TheActivity.kt
@@ -11,96 +11,73 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
+package com.example.perfectcamerademo
 
-package com.example.perfectcamerademo;
+import android.app.Activity
+import android.graphics.PixelFormat
+import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
 
-import android.app.Activity;
-import android.graphics.PixelFormat;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+class TheActivity : Activity(), SurfaceHolder.Callback {
+    private val yolov6ncnn = Yolo6()
+    private var facing = 0
+    private var cameraView: SurfaceView? = null
 
-public class TheActivity extends Activity implements SurfaceHolder.Callback
-{
+    /** Called when the activity is first created.  */
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
 
-    private Yolo6 yolov6ncnn = new Yolo6();
-    private int facing = 0;
-    private SurfaceView cameraView;
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        cameraView = findViewById<View>(R.id.cameraview) as SurfaceView
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        cameraView!!.holder.setFormat(PixelFormat.RGBA_8888)
+        cameraView!!.holder.addCallback(this)
 
-        cameraView = (SurfaceView) findViewById(R.id.cameraview);
+        val buttonSwitchCamera = findViewById<View>(R.id.buttonSwitchCamera) as Button
+        buttonSwitchCamera.setOnClickListener {
+            val new_facing = 1 - facing
+            yolov6ncnn.closeCamera()
 
-        cameraView.getHolder().setFormat(PixelFormat.RGBA_8888);
-        cameraView.getHolder().addCallback(this);
+            yolov6ncnn.openCamera(new_facing, 0)
+            facing = new_facing
+        }
 
-        Button buttonSwitchCamera = (Button) findViewById(R.id.buttonSwitchCamera);
-        buttonSwitchCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-
-                int new_facing = 1 - facing;
-
-                yolov6ncnn.closeCamera();
-
-                yolov6ncnn.openCamera(new_facing, 0);
-
-                facing = new_facing;
-            }
-        });
-
-        reload();
+        reload()
     }
 
-    private void reload()
-    {
-        boolean ret_init = yolov6ncnn.loadModel(getAssets(), 0, 0);
-        if (!ret_init)
-        {
-            Log.e("MainActivity", "yolov6ncnn loadModel failed");
+    private fun reload() {
+        val ret_init = yolov6ncnn.loadModel(assets, 0, 0)
+        if (!ret_init) {
+            Log.e("MainActivity", "yolov6ncnn loadModel failed")
         }
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
-    {
-        yolov6ncnn.setOutputWindow(holder.getSurface());
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        yolov6ncnn.setOutputWindow(holder.surface)
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder)
-    {
+    override fun surfaceCreated(holder: SurfaceHolder) {
     }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder)
-    {
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
     }
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
+    public override fun onResume() {
+        super.onResume()
 
-        yolov6ncnn.openCamera(1, 0);
+        yolov6ncnn.openCamera(1, 0)
     }
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
+    public override fun onPause() {
+        super.onPause()
 
-        yolov6ncnn.closeCamera();
+        yolov6ncnn.closeCamera()
     }
 }
