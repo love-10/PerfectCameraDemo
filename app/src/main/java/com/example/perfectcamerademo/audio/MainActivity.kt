49f6b27707d72dda08630540739c413e2eb6927c
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-08-08 23:15:25 +0800

    yuv

--- app/src/main/java/com/example/perfectcamerademo/audio/MainActivity.kt
+++ app/src/main/java/com/example/perfectcamerademo/audio/MainActivity.kt
@@ -1,128 +1,41 @@
 package com.example.perfectcamerademo.audio
 
-import android.content.Intent
-import android.media.AudioFormat
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.fragment.app.FragmentActivity
-import com.example.perfectcamerademo.App
 import com.example.perfectcamerademo.App.Companion.instance
 import com.example.perfectcamerademo.R
+import com.example.perfectcamerademo.databinding.ActivityAudioBinding
 import com.zlw.main.recorderlib.RecordManager
 import com.zlw.main.recorderlib.recorder.RecordConfig
 import com.zlw.main.recorderlib.recorder.RecordHelper.RecordState
 import com.zlw.main.recorderlib.recorder.listener.RecordStateListener
-import org.example.SingleKalman.log
 import java.util.Locale
 
-class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, View.OnClickListener {
-    var btRecord: Button? = null
-    var btStop: Button? = null
-    var tvState: TextView? = null
-    var tvSoundSize: TextView? = null
-    var rgAudioFormat: RadioGroup? = null
-    var rgSimpleRate: RadioGroup? = null
-    var tbEncoding: RadioGroup? = null
-    var tbSource: RadioGroup? = null
-    var audioView: AudioView? = null
-    var spUpStyle: Spinner? = null
-    var spDownStyle: Spinner? = null
+class MainActivity : FragmentActivity(), View.OnClickListener {
 
     private var isStart = false
     private var isPause = false
     private val recordManager: RecordManager = RecordManager.getInstance()
+    private lateinit var binding: ActivityAudioBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audio)
+        binding = ActivityAudioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initView()
-        initAudioView()
-        initEvent()
         initRecord()
     }
 
     private fun initView() {
-        btRecord = findViewById(R.id.btRecord)
-        btStop = findViewById(R.id.btStop)
-        tvState = findViewById(R.id.tvState)
-        btRecord = findViewById(R.id.btRecord)
-        tvSoundSize = findViewById(R.id.tvSoundSize)
-        rgAudioFormat = findViewById(R.id.rgAudioFormat)
-        rgSimpleRate = findViewById(R.id.rgSimpleRate)
-        tbEncoding = findViewById(R.id.tbEncoding)
-        audioView = findViewById(R.id.audioView)
-        spUpStyle = findViewById(R.id.spUpStyle)
-        spDownStyle = findViewById(R.id.spDownStyle)
-        tbSource = findViewById(R.id.tbSource)
-        btRecord!!.setOnClickListener(this)
-        btStop!!.setOnClickListener(this)
-        findViewById<View>(R.id.jumpTestActivity).setOnClickListener(this)
+        binding.btRecord.setOnClickListener(this)
+        binding.btStop.setOnClickListener(this)
     }
 
     override fun onResume() {
         super.onResume()
         initRecordEvent()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    private fun initAudioView() {
-        tvState!!.visibility = View.GONE
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, STYLE_DATA)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spUpStyle!!.adapter = adapter
-        spDownStyle!!.adapter = adapter
-        spUpStyle!!.onItemSelectedListener = this
-        spDownStyle!!.onItemSelectedListener = this
-    }
-
-    private fun initEvent() {
-        rgAudioFormat!!.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == R.id.rbPcm) {
-                recordManager.changeFormat(RecordConfig.RecordFormat.PCM)
-            } else if (checkedId == R.id.rbMp3) {
-                recordManager.changeFormat(RecordConfig.RecordFormat.MP3)
-            } else if (checkedId == R.id.rbWav) {
-                recordManager.changeFormat(RecordConfig.RecordFormat.WAV)
-            }
-        }
-
-        rgSimpleRate!!.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == R.id.rb8K) {
-                recordManager.changeRecordConfig(recordManager.recordConfig.setSampleRate(8000))
-            } else if (checkedId == R.id.rb16K) {
-                recordManager.changeRecordConfig(recordManager.recordConfig.setSampleRate(16000))
-            } else if (checkedId == R.id.rb44K) {
-                recordManager.changeRecordConfig(recordManager.recordConfig.setSampleRate(44100))
-            }
-        }
-
-        tbEncoding!!.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == R.id.rb8Bit) {
-                recordManager.changeRecordConfig(
-                    recordManager.recordConfig.setEncodingConfig(
-                        AudioFormat.ENCODING_PCM_8BIT
-                    )
-                )
-            } else if (checkedId == R.id.rb16Bit) {
-                recordManager.changeRecordConfig(
-                    recordManager.recordConfig.setEncodingConfig(
-                        AudioFormat.ENCODING_PCM_16BIT
-                    )
-                )
-            }
-        }
-        tbSource!!.setOnCheckedChangeListener { group, checkedId -> }
     }
 
     private fun initRecord() {
@@ -136,13 +49,13 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
         recordManager.setRecordStateListener(object : RecordStateListener {
             override fun onStateChange(state: RecordState) {
                 when (state) {
-                    RecordState.PAUSE -> tvState!!.text = "暂停中"
-                    RecordState.IDLE -> tvState!!.text = "空闲中"
-                    RecordState.RECORDING -> tvState!!.text = "录音中"
-                    RecordState.STOP -> tvState!!.text = "停止"
+                    RecordState.PAUSE -> binding.tvState.text = "暂停中"
+                    RecordState.IDLE -> binding.tvState.text = "空闲中"
+                    RecordState.RECORDING -> binding.tvState.text = "录音中"
+                    RecordState.STOP -> binding.tvState.text = "停止"
                     RecordState.FINISH -> {
-                        tvState!!.text = "录音结束"
-                        tvSoundSize!!.text = "---"
+                        binding.tvState.text = "录音结束"
+                        binding.tvSoundSize.text = "---"
                     }
 
                     else -> {}
@@ -154,7 +67,7 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
             }
         })
         recordManager.setRecordSoundSizeListener { soundSize ->
-            tvSoundSize!!.text = String.format(
+            binding.tvSoundSize.text = String.format(
                 Locale.getDefault(), "声音大小：%s db", soundSize
             )
         }
@@ -165,7 +78,6 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
                 Toast.LENGTH_SHORT
             ).show()
         }
-        recordManager.setRecordFftDataListener { data -> audioView!!.setWaveData(data) }
     }
 
     override fun onClick(view: View) {
@@ -179,7 +91,7 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
 
     private fun doStop() {
         recordManager.stop()
-        btRecord!!.text = "开始"
+        binding.btRecord.text = "开始"
         isPause = false
         isStart = false
     }
@@ -187,7 +99,7 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
     private fun doPlay() {
         if (isStart) {
             recordManager.pause()
-            btRecord!!.text = "开始"
+            binding.btRecord.text = "开始"
             isPause = true
             isStart = false
         } else {
@@ -196,35 +108,8 @@ class MainActivity : FragmentActivity(), AdapterView.OnItemSelectedListener, Vie
             } else {
                 recordManager.start()
             }
-            btRecord!!.text = "暂停"
+            binding.btRecord.text = "暂停"
             isStart = true
         }
-    }
-
-
-    override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-        val parentId = parent.id
-        if (parentId == R.id.spUpStyle) {
-            audioView!!.setStyle(
-                AudioView.ShowStyle.getStyle(STYLE_DATA[position]),
-                audioView!!.downStyle
-            )
-        } else if (parentId == R.id.spDownStyle) {
-            audioView!!.setStyle(
-                audioView!!.upStyle,
-                AudioView.ShowStyle.getStyle(STYLE_DATA[position])
-            )
-        }
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        //nothing
-    }
-
-    companion object {
-        private val TAG: String = MainActivity::class.java.simpleName
-
-        private val STYLE_DATA =
-            arrayOf("STYLE_ALL", "STYLE_NOTHING", "STYLE_WAVE", "STYLE_HOLLOW_LUMP")
     }
 }
