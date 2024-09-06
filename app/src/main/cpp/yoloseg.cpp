f443ad228f835909e8330b1cd45c3f34e6a698b9
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-09-07 00:29:01 +0800

    yuv

--- app/src/main/cpp/yoloseg.cpp
+++ app/src/main/cpp/yoloseg.cpp
@@ -629,55 +629,55 @@ int YoloSeg::draw(cv::Mat &rgb, const std::vector<Object> &objects, bool needMar
 //    channels[2] += brightSpots;  // 添加到红色通道
 //    cv::merge(channels, rgb);  // 合并通道
 
-    for (size_t i = 0; i < objects.size(); i++) {
-        const Object &obj = objects[i];
-        const unsigned char *color = colors[color_index % 80];
-        color_index++;
-
-        cv::Scalar cc(color[0], color[1], color[2]);
-
-        fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
-                obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
-        if (needMark) {
-            for (int y = 0; y < rgb.rows; y++) {
-                uchar *image_ptr = rgb.ptr(y);
-                const float *mask_ptr = obj.mask.ptr<float>(y);
-                for (int x = 0; x < rgb.cols; x++) {
-                    if (mask_ptr[x] >= 0.5) {
-                        image_ptr[0] = cv::saturate_cast<uchar>(
-                                image_ptr[0] * 0.5 + color[2] * 0.5);
-                        image_ptr[1] = cv::saturate_cast<uchar>(
-                                image_ptr[1] * 0.5 + color[1] * 0.5);
-                        image_ptr[2] = cv::saturate_cast<uchar>(
-                                image_ptr[2] * 0.5 + color[0] * 0.5);
-                    }
-                    image_ptr += 3;
-                }
-            }
-        }
-
-        cv::rectangle(rgb, obj.rect, cc, 2);
-
-        char text[256];
-        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
-
-        int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-
-        int x = obj.rect.x;
-        int y = obj.rect.y - label_size.height - baseLine;
-        if (y < 0)
-            y = 0;
-        if (x + label_size.width > rgb.cols)
-            x = rgb.cols - label_size.width;
-
-        cv::rectangle(rgb, cv::Rect(cv::Point(x, y),
-                                    cv::Size(label_size.width, label_size.height + baseLine)),
-                      cv::Scalar(255, 255, 255), -1);
-
-        cv::putText(rgb, text, cv::Point(x, y + label_size.height),
-                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-    }
+//    for (size_t i = 0; i < objects.size(); i++) {
+//        const Object &obj = objects[i];
+//        const unsigned char *color = colors[color_index % 80];
+//        color_index++;
+//
+//        cv::Scalar cc(color[0], color[1], color[2]);
+//
+//        fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
+//                obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
+//        if (needMark) {
+//            for (int y = 0; y < rgb.rows; y++) {
+//                uchar *image_ptr = rgb.ptr(y);
+//                const float *mask_ptr = obj.mask.ptr<float>(y);
+//                for (int x = 0; x < rgb.cols; x++) {
+//                    if (mask_ptr[x] >= 0.5) {
+//                        image_ptr[0] = cv::saturate_cast<uchar>(
+//                                image_ptr[0] * 0.5 + color[2] * 0.5);
+//                        image_ptr[1] = cv::saturate_cast<uchar>(
+//                                image_ptr[1] * 0.5 + color[1] * 0.5);
+//                        image_ptr[2] = cv::saturate_cast<uchar>(
+//                                image_ptr[2] * 0.5 + color[0] * 0.5);
+//                    }
+//                    image_ptr += 3;
+//                }
+//            }
+//        }
+//
+//        cv::rectangle(rgb, obj.rect, cc, 2);
+//
+//        char text[256];
+//        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
+//
+//        int baseLine = 0;
+//        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+//
+//        int x = obj.rect.x;
+//        int y = obj.rect.y - label_size.height - baseLine;
+//        if (y < 0)
+//            y = 0;
+//        if (x + label_size.width > rgb.cols)
+//            x = rgb.cols - label_size.width;
+//
+//        cv::rectangle(rgb, cv::Rect(cv::Point(x, y),
+//                                    cv::Size(label_size.width, label_size.height + baseLine)),
+//                      cv::Scalar(255, 255, 255), -1);
+//
+//        cv::putText(rgb, text, cv::Point(x, y + label_size.height),
+//                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+//    }
 
     return 0;
 }
